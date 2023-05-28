309b3ddfe9f0ed3d3126195b5377744c38063732
Gustavo Ferreira <gustavo_cosme@id.uff.br>
2023-05-28 17:45:28 -0300

    cria metodo gerarRelatorio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import relatorio.Relatorio;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Relatorio relatorio = Relatorio.getInstance();
+        relatorio.gerarRelatorio();
     }
 }
