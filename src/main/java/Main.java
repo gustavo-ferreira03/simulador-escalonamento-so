8a6a91b94f1fdd24d048176d33ce1382df187054
Gustavo Ferreira <gustavo_cosme@id.uff.br>
2023-05-28 22:12:18 -0300

    remove pasta interna main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
-import componentes.Simulador;
 import relatorio.Relatorio;
 
 public class Main {
     public static void main(String[] args) {
-        Simulador simulador = new Simulador("arquivoTeste.txt");
-        simulador.iniciar();
+        Relatorio relatorio = Relatorio.getInstance();
+        relatorio.gerarRelatorio();
     }
 }
