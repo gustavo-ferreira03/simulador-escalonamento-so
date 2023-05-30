708a7d43146ae1eaa5701f97ec919146dc7218c3
Gustavo Ferreira <gustavo_cosme@id.uff.br>
2023-05-30 15:52:03 -0300

    implementa versao inicial do simulador (sem io)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
+import componentes.Simulador;
 import relatorio.Relatorio;
 
 public class Main {
     public static void main(String[] args) {
-        Relatorio relatorio = Relatorio.getInstance();
-        relatorio.gerarRelatorio();
+        Simulador simulador = new Simulador("arquivoTeste.txt");
+        simulador.iniciar();
     }
 }
--- src/main/java/componentes/Cpu.java
+++ src/main/java/componentes/Cpu.java
@@ -2,9 +2,60 @@
 package componentes;
 
 public class Cpu {
-    public Processo processoAtual;
+    int contador;
+    private final SistemaOperacional so;
+    private Processo processoAtual;
 
-    public Cpu(Processo processoAtual) {
-        this.processoAtual = processoAtual;
+    Cpu(SistemaOperacional so) {
+        this.contador = 0;
+        this.so = so;
+    }
+
+    public void executar() {
+        if(this.processoAtual == null) {
+            if(this.so.getEscalonador().temProcesso()){
+                obterProximoProcesso();
+            }
+            else {
+                return;
+            }
+        }
+        this.processoAtual.executar();
+        this.contador++;
+
+        if(this.processoAtual.verificarConclusao()) {
+            System.out.println("PROCESSO ATUAL: " + this.processoAtual.getPrioridade());
+            obterProximoProcesso();
+            this.contador = 0;
+            return;
+        }
+        if(verificarPreempcao()) {
+            this.so.getEscalonador().adicionarProcesso(this.processoAtual);
+            obterProximoProcesso();
+            this.contador = 0;
+
+        }
+    }
+
+    public boolean ociosa() {
+        return this.processoAtual == null;
+    }
+
+    private boolean verificarPreempcao() {
+        if(this.processoAtual.getPrioridade() == Prioridade.TEMPO_REAL) {
+            return false;
+        }
+        if(this.so.getEscalonador().temProcesso()) {
+            return this.contador >= this.so.getEscalonador().getQuantum();
+        }
+        return false;
+    }
+
+    private boolean obterProximoProcesso() {
+        this.processoAtual = this.so.getEscalonador().obterProximoProcesso();
+        if(this.processoAtual != null) {
+            return true;
+        }
+        return false;
     }
 }
