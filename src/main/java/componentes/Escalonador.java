75741cf1123a2acb8ed27f3158e98d14f9ef229e
Gustavo Ferreira <gustavo_cosme@id.uff.br>
2023-05-30 15:52:03 -0300

    implementa versao inicial do simulador (sem io)

--- src/main/java/componentes/Escalonador.java
+++ src/main/java/componentes/Escalonador.java
@@ -1,21 +1,19 @@
 package componentes;
 
-import relatorio.FilasRelatorio;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
 public class Escalonador {
-    private SistemaOperacional so;
     private int quantum;
+    private Queue<Processo> filaIo;
     private Queue<Processo> filaReal;
     private List<Queue<Processo>> filasUsuario;
 
-    Escalonador(SistemaOperacional so, int quantum) {
-        this.so = so;
+    Escalonador(int quantum) {
         this.quantum = quantum;
+        this.filaIo = new ArrayDeque<>();
         this.filaReal = new ArrayDeque<>();
         this.filasUsuario = new ArrayList<>();
         for(int i = 0; i < 3; i++) {
@@ -40,7 +38,6 @@ public class Escalonador {
     }
 
     public void adicionarProcesso(Processo processo) {
-        System.out.println("PROCESSO PRONTO: " + processo.getNome());
         if(processo.getPrioridade() == Prioridade.TEMPO_REAL) {
             this.filaReal.offer(processo);
         }
@@ -52,43 +49,13 @@ public class Escalonador {
 
     public Processo obterProximoProcesso() {
         if(!filaReal.isEmpty()) {
-            System.out.println("PROCESSO ESCALONADO: " + filaReal.peek().getNome());
             return filaReal.poll();
         }
         for(Queue<Processo> filaUsuario : filasUsuario) {
             if(!filaUsuario.isEmpty()) {
-                System.out.println("PROCESSO ESCALONADO: " + filaUsuario.peek().getNome());
                 return filaUsuario.poll();
             }
         }
         return null;
-    }
-
-    public SistemaOperacional getSo() {
-        return so;
-    }
-
-    public void setSo(SistemaOperacional so) {
-        this.so = so;
-    }
-
-    public void setQuantum(int quantum) {
-        this.quantum = quantum;
-    }
-
-    public Queue<Processo> getFilaReal() {
-        return filaReal;
-    }
-
-    public void setFilaReal(Queue<Processo> filaReal) {
-        this.filaReal = filaReal;
-    }
-
-    public List<Queue<Processo>> getFilasUsuario() {
-        return filasUsuario;
-    }
-
-    public void setFilasUsuario(List<Queue<Processo>> filasUsuario) {
-        this.filasUsuario = filasUsuario;
     }
 }
