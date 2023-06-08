38d15c5add949d198359e960f09487caeb0bae9e
Gustavo Ferreira <gustavo_cosme@id.uff.br>
2023-06-08 16:59:43 -0300

    adiciona progresso ao relatorio

--- src/main/java/relatorio/ProgressoRelatorio.java
+++ src/main/java/relatorio/ProgressoRelatorio.java
@@ -1,4 +1,13 @@
 package relatorio;
 
+import java.util.Map;
+
 public class ProgressoRelatorio {
+    int tempoDecorrido;
+    int tempoCpu;
+
+    public ProgressoRelatorio(int tempoDecorrido, int tempoCpu) {
+        this.tempoDecorrido = tempoDecorrido;
+        this.tempoCpu = tempoCpu;
+    }
 }
